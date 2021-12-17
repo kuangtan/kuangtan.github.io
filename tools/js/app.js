9d6d21e546f098db804ecdf7b55ae32404cf952d
kuangtant <kuangtant@163.com>
2021-12-17 20:51:17 +0800

    Site updated: 2021-12-17 20:51:16

---
+++ tools/js/app.js
@@ -0,0 +1,6 @@
+var _hmt = _hmt || [];
+$(function(){var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?32393490d19d39bf873397a5d753f085";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+});
